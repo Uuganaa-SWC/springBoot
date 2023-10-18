1defbc176e7a0c56906a89cb84fe6789cf2e304c
uuganbat.d <uuganbat.d@arigbank.mn>
2023-10-18 11:03:07 +0800

    added

---
+++ src/main/java/arigbank/mn/FirstProject/HelloWorldController.java
@@ -0,0 +1,39 @@
+package arigbank.mn.FirstProject;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+
+@RestController
+public class HelloWorldController {
+
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+    }
+
+}
---
+++ src/main/java/arigbank/mn/FirstProject/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package arigbank.mn.FirstProject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+}
